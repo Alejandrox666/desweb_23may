62a00de22ad2d8701092675d7a41d8705c0606a8
Alejandrox666 <116208108+Alejandrox666@users.noreply.github.com>
2025-05-23 19:00:21 -0600

    version iicial

---
+++ src/componentes/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+function App() {
+   return(
+       <>
+       <Navbar />
+        <div>
+           <h2>Bienvenido a mi página Web</h2>
+           <hr />
+           <p>
+               Este simple Aplicación es creado con React, De ALejandro Hernández Negrete
+           </p>
+       </div>
+       </>
+   )
+
+
+}
+
+
+export default App;
